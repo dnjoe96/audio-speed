8fd879e0661110e941a43d81b4e5697da8ba2426
dnjoe96 <donjoedbest@gmail.com>
2022-03-13 08:42:17 +0100

    [feat] Add Commandline interface

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
+#!/usr/bin/env python3
 import click
 import json
 import requests
-from test import auth_key
 
+# eventually, I would have to call getenv from the environment
 auth_key = '782ad5e3c67d4aaa9844057332d0591e'
 endpoint = "https://api.assemblyai.com/v2/transcript"
-
-
-
-"""
-endpoint = "https://api.assemblyai.com/v2/transcript"
-json = {
-    "audio_url": "https://bit.ly/3yxKEIY"
-}
-headers = {
-    "authorization": "YOUR-API-TOKEN",
-    "content-type": "application/json"
-}
-response = requests.post(endpoint, json=json, headers=headers)
-print(response.json())
-
-
-############# GET RESULT ##################
-import requests
-endpoint = "https://api.assemblyai.com/v2/transcript/YOUR-TRANSCRIPT-ID-HERE"
-headers = {
-    "authorization": "YOUR-API-TOKEN",
-}
-response = requests.get(endpoint, headers=headers)
-print(response.json())
-
-"""
-
 
 headers = {
     "authorization": auth_key,
     "content-type": "application/json"
 }
+
+
+def is_link(string):
+    if string is None:
+        return False
+    if 'http://' in string or 'https://' in string:
+        return True
+    else:
+        return False
 
 
 def read_file(filename):
@@ -48,55 +31,64 @@ def read_file(filename):
             yield data
 
 
-# with this url, we upload audio file
-upload_response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers,
-                                data=read_file('test_audio.mp3'))
-audio_url = upload_response.json()["upload_url"]
+def link_handler(link):
+    json = {
+        "audio_url": link
+    }
+    response = requests.post(endpoint, json=json, headers=headers)
+    print('url posted')
+    return response.json()
 
-transcript_request = {'audio_url': audio_url}
-transcript_response = requests.post("https://api.assemblyai.com/v2/transcript",
-                                    json=transcript_request, headers=headers)
-# print(transcript_response.json())
 
-_id = transcript_response.json()["id"]
+def media_file_handler(file):
+    # with this url, we upload audio file
+    upload_response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers,
+                                    data=read_file('test_audio.mp3'))
+    print('file uploaded')
+    audio_url = upload_response.json()["upload_url"]
 
-endpoint = "https://api.assemblyai.com/v2/transcript/{}".format(_id)
-# print(endpoint)
+    return link_handler(audio_url)
 
-# print('===========================')
 
-headers = {
-    "authorization": auth_key,
-}
-
-reponse_status = transcript_response.json()['status']
-while reponse_status != 'completed':
-    # print(reponse_status)
+def get_result(_id, status):
+    headers = {"authorization": auth_key}
+    endpoint = "https://api.assemblyai.com/v2/transcript/{}".format(_id)
     response = requests.get(endpoint, headers=headers)
-    reponse_status = response.json()['status']
 
-
-print(json.dumps(response.json(), indent=2))
-# print(response.json())
-
-
+    print('processing .', end="")
+    while status != 'completed':
+        print('.', end="")
+        response = requests.get(endpoint, headers=headers)
+        status = response.json()['status']
+    print('\ndone')
+    return len(response.json()['words']), response.json()['audio_duration']
 
 
 supported_files = ".3ga .aac .ac3 .aif .aiff .alac .amr .ape .au .dss .flac " \
                   ".flv .m4a .m4b .m4p .mp3 .mpga .ogg, .oga, .mogg .opus .qcp " \
                   ".tta .voc .wav .wv"
 
-_help = 'audio file or audio url (http).\n' \
-        'Supported file formats are: \n ' + supported_files
+_help = 'pass audio file in the format\n' \
+        '-f [filename | link-url] ' \
+        '\t audio file or audio url (http).\n' \
+        '\t Supported file formats are: \n \t' + supported_files
 
 
 @click.command()
 # @click.argument('audio_file')
 @click.option('-f', '--file', default=None, help=_help)
 def getspeed(file):
-    click.echo('Hello World!' + file)
+    if file is None:
+        click.echo(_help)
+        return
+    if is_link(file):
+        response = link_handler(file)
+    else:
+        response = media_file_handler(file)
+
+    res = get_result(response['id'], response['status'])
+    click.echo(res)
 
 
 if __name__ == '__main__':
     getspeed()
-    # print(help_file)
